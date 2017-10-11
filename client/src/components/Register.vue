661a81ad84717318466d0d48a7afe8391740951b
Shoaib Rafiq <Shoaib@Shoaibs-MacBook-Air.local>
2017-10-11 21:08:30 +0100

    setting uo registration page

---
+++ client/src/components/Register.vue
@@ -0,0 +1,49 @@
+<template>
+<div>
+<h1>Regsiter</h1>
+
+ <input 
+ type="email" 
+ v-model="email"
+ name="email"
+ placeholder="Email" />
+<br>
+ <input 
+ type="password" 
+ v-model="password"
+ name="password"
+ placeholder="Password" />
+ <br>
+ <button 
+ @click="register">
+ Register
+ </button>
+</div>
+  
+   
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
